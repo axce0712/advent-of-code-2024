a85edad395491f47b9528dba0618a837e0086f74
axce0712 <christoph7@gmail.com>
2024-12-03 14:56:41 +0100

    Simpify regex for day 3

--- day03.fsx
+++ day03.fsx
@@ -11,15 +11,15 @@ partOne "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 let partTwo input =
     let mutable enabled = true
     let mutable acc = 0
-    for entry in Regex.Matches(input, @"((mul)\((\d+)\,(\d+)\))|((don't)\(\))|((do)\(\))") do
-        if entry.Groups[2].Value = "mul" then
+    for entry in Regex.Matches(input, @"(mul)\((\d+)\,(\d+)\)|(don't)\(\)|(do)\(\)") do
+        if entry.Groups[1].Value = "mul" then
             if enabled then
-                let x = entry.Groups[3].Value |> int
-                let y = entry.Groups[4].Value |> int
+                let x = entry.Groups[2].Value |> int
+                let y = entry.Groups[3].Value |> int
                 acc <- acc + x * y
-        else if entry.Groups[6].Value = "don't" then
+        else if entry.Groups[4].Value = "don't" then
             enabled <- false
-        else if entry.Groups[8].Value = "do" then
+        else if entry.Groups[5].Value = "do" then
             enabled <- true
         else
             failwithf "Unable to handle %A" entry
