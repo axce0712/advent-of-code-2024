db7b2d882915f23b9ad0053badaa233f91d98778
chris <christoph7@gmail.com>
2024-12-15 16:05:50 +0100

    Format day 12

--- day12.fsx
+++ day12.fsx
@@ -2,24 +2,29 @@ open System.IO
 
 type Plant = char
 
-type Garden = Plant[,]
+type Garden = Plant [,]
 
 module Garden =
     let height (garden: Garden) = Array2D.length1 garden
-    
+
     let width (garden: Garden) = Array2D.length2 garden
 
-    let item (x, y) (garden: Garden) =
-        garden[y, x]
+    let item (x, y) (garden: Garden) = garden[y, x]
 
     let itemOrFallback (x, y) fallback (garden: Garden) =
-        if x >= 0 && x < Array2D.length2 garden && y >= 0 && y < Array2D.length1 garden then
+        if x >= 0
+           && x < Array2D.length2 garden
+           && y >= 0
+           && y < Array2D.length1 garden then
             garden[y, x]
         else
             fallback
 
     let isInRange (x, y) (garden: Garden) =
-        x >= 0 && x < Array2D.length2 garden && y >= 0 && y < Array2D.length1 garden
+        x >= 0
+        && x < Array2D.length2 garden
+        && y >= 0
+        && y < Array2D.length1 garden
 
 let parse (lines: seq<string>) : Garden = array2D lines
 
@@ -30,9 +35,8 @@ let findArea plant (sx, sy) (garden: Garden) =
         let nx, ny = x + dx, y + dy
 
         if Garden.isInRange (nx, ny) garden
-            && not (area |> Set.contains (nx, ny))
-            && Garden.item (nx, ny) garden = plant
-        then
+           && not (area |> Set.contains (nx, ny))
+           && Garden.item (nx, ny) garden = plant then
             (nx, ny) :: queue, Set.add (nx, ny) area
         else
             queue, area
@@ -58,17 +62,17 @@ let findAreas (garden: Garden) =
 
             areasSoFar
             |> Map.change plant (function
-                | Some areasSoFar -> Some (area :: areasSoFar)
+                | Some areasSoFar -> Some(area :: areasSoFar)
                 | None -> Some [ area ])
 
     let xs = Seq.init (Garden.width garden) id
     let ys = Seq.init (Garden.height garden) id
+
     let positions =
         ys
         |> Seq.collect (fun y -> xs |> Seq.map (fun x -> x, y))
-    
-    positions
-    |> Seq.fold folder Map.empty
+
+    positions |> Seq.fold folder Map.empty
 
 let getRow (y: int) (start: int) (length: int) (garden: Garden) =
     seq {
@@ -82,8 +86,7 @@ let getColumn (x: int) (start: int) (length: int) (garden: Garden) =
             Garden.itemOrFallback (x, y) '.' garden
     }
 
-let (|Even|Uneven|) (p1, p2) =
-    if p1 = p2 then Even else Uneven
+let (|Even|Uneven|) (p1, p2) = if p1 = p2 then Even else Uneven
 
 let perimeter (before1, before2, positions) (current1, current2) =
     let b1, _ = before1
@@ -96,25 +99,27 @@ let perimeter (before1, before2, positions) (current1, current2) =
 
     current1, current2, newPositions
 
-let findDistinctSides (before1, before2, positions) (current1, current2) =
+let distinctSides (before1, before2, positions) (current1, current2) =
     let b1, (bx1, by1) = before1
     let b2, (bx2, by2) = before2
     let p1, _ = current1
     let p2, _ = current2
+
     let newPositions =
         match (b1, b2), (p1, p2) with
         | Uneven, Uneven ->
             let mutable newPositions = positions
+
             if b1 <> p1 then
                 newPositions <- (b1, (bx1, by1)) :: newPositions
-            
+
             if b2 <> p2 then
                 newPositions <- (b2, (bx2, by2)) :: newPositions
-                
+
             newPositions
         | Uneven, Even -> (b1, (bx1, by1)) :: (b2, (bx2, by2)) :: positions
         | _ -> positions
-        
+
     current1, current2, newPositions
 
 let findHorizontalSides f y (garden: Garden) =
@@ -123,11 +128,12 @@ let findHorizontalSides f y (garden: Garden) =
     let rowWithPositions1 = Seq.zip row1 (Seq.initInfinite (fun x -> (x, y - 1)))
     let rowWithPositions2 = Seq.zip row2 (Seq.initInfinite (fun x -> (x, y)))
     let initial1 = '.', (-1, y - 1)
-    let initial2 = '.',  (-1, y)
+    let initial2 = '.', (-1, y)
+
     let _, _, positions =
         Seq.zip rowWithPositions1 rowWithPositions2
         |> Seq.fold f (initial1, initial2, [])
-        
+
     positions
 
 let findVerticalSides f x (garden: Garden) =
@@ -136,48 +142,54 @@ let findVerticalSides f x (garden: Garden) =
     let columnWithPositions1 = Seq.zip column1 (Seq.initInfinite (fun y -> (x - 1, y)))
     let columnWithPositions2 = Seq.zip column2 (Seq.initInfinite (fun y -> (x, y)))
     let initial1 = '.', (x - 1, -1)
-    let initial2 = '.',  (x, -1)
+    let initial2 = '.', (x, -1)
+
     let _, _, positions =
         Seq.zip columnWithPositions1 columnWithPositions2
         |> Seq.fold f (initial1, initial2, [])
-        
+
     positions
 
 let countSides f (garden: Garden) =
-    let count = function | Some x -> Some (x + 1) | None -> Some 1
+    let count =
+        function
+        | Some x -> Some(x + 1)
+        | None -> Some 1
 
-    Seq.collect id [
-        for y in 0 .. Garden.height garden do
-            findHorizontalSides f y garden
-        for x in 0 .. Garden.width garden do
-            findVerticalSides f x garden
-    ]
+    Seq.collect
+        id
+        [ for y in 0 .. Garden.height garden do
+              findHorizontalSides f y garden
+          for x in 0 .. Garden.width garden do
+              findVerticalSides f x garden ]
     |> Seq.fold (fun acc current -> acc |> Map.change current count) Map.empty
 
 let solve f (garden: Garden) =
     let regions = findAreas garden
     let numberOfSides = countSides f garden
+
     let totalPrice =
         Map.toSeq regions
         |> Seq.sumBy (fun (_, areas) ->
             areas
             |> List.sumBy (fun positions ->
                 let area = Set.count positions
+
                 let sides =
                     positions
                     |> Seq.sumBy (fun pos ->
                         let key = Garden.item pos garden, pos
-                        Map.tryFind key numberOfSides |> Option.defaultValue 0)
-                
+
+                        Map.tryFind key numberOfSides
+                        |> Option.defaultValue 0)
+
                 area * sides))
 
     totalPrice
 
-let partOne (garden: Garden) =
-    solve perimeter garden
-    
-let partTwo (garden: Garden) =
-    solve findDistinctSides garden
+let partOne (garden: Garden) = solve perimeter garden
+
+let partTwo (garden: Garden) = solve distinctSides garden
 
 let lines = File.ReadLines("./input/day12.txt")
 
